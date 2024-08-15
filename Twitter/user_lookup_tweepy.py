6bc641a3c557c7dc76c4b25033f5f637f1ea0a40
LVKinyanjui <leslievictorius@gmail.com>
2024-08-15 16:10:11 +0300

    Refactored lookup with user context to extract basic data from user

--- Twitter/user_lookup_tweepy.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-import os
-
-client = tweepy.Client(bearer_token=os.getenv('TWITTER_BEARER_TOKEN'))
-
-reponse = client.get_user(username='lii_karis', user_fields=["created_at"])
-print(response.data)
-
