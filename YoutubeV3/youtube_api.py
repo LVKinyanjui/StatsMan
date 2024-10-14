cb9282a0c2d54944137f2abf7da78abbefc5559b
LVKinyanjui <leslievictorius@gmail.com>
2024-10-14 13:57:20 +0300

    Implemented full algorithm; trouble getting channel id from username; requests failure

--- YoutubeV3/youtube_api.py
+++ YoutubeV3/youtube_api.py
@@ -6,9 +6,19 @@ class YoutubeAPI:
     def __init__(self, channel_username: str):
         api_key = os.getenv("YOUTUBE_API_KEY")
         self.youtube = build("youtube", "v3", developerKey=api_key)
+        
+        # In this order
         self.username = channel_username
+        self.channel_id = self._get_channel_id_by_username()     # Trivial and necessary for all subsequent operations
 
-    def get_channel_id_by_username(self):
+        # Attributes initially set to none as changes to class structure are expectes
+        # and may not be necessary to call their methods in the constructor
+        # as some may be optional and others long running
+        self.uploads_playlist_id = None
+        self.video_ids = None
+        self.video_stats = None
+
+    def _get_channel_id_by_username(self):
         request = self.youtube.channels().list(
             part="id",
             forUsername=self.username
@@ -16,12 +26,73 @@ class YoutubeAPI:
         response = request.execute()
 
         try:
-            return response['items'][0]['id']
+            self.channel_id = response['items'][0]['id']
+            return self
         except KeyError:
             raise SystemExit(f"No channel found for username: {self.username} \nTry Another")
 
+    def get_uploads_playlist_id(self):
+        request = self.youtube.channels().list(
+            part="contentDetails",
+            id=self.channel_id
+        )
+        response = request.execute()
+
+        self.uploads_playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        return self
+    
+    def get_video_ids_from_playlist(self) -> list:
+        video_ids = []
+        next_page_token = None
+
+        # TODO: Perfom each request asynchronously to reduce wait times
+        while True:
+            request = self.youtube.playlistItems().list(
+                part="snippet",
+                playlistId=self.uploads_playlist_id,
+                maxResults=50,
+                pageToken=next_page_token
+            )
+            response = request.execute()
+
+            for item in response['items']:
+                video_id = item['snippet']['resourceId']['videoId']
+                video_ids.append(video_id)
+
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+        self.video_ids = video_ids
+        return self
+
+    def get_video_stats(self):
+        video_stats = []
+        for i in range(0, len(self.video_ids), 50):  # YouTube API accepts up to 50 video IDs per request
+            request = self.youtube.videos().list(
+                part="statistics,contentDetails",
+                id=','.join(self.video_ids[i:i+50])
+            )
+            response = request.execute()
+
+            for item in response['items']:
+                stats = {
+                    'videoId': item['id'],
+                    'viewCount': item['statistics'].get('viewCount', 0),
+                    'likeCount': item['statistics'].get('likeCount', 0),
+                    'commentCount': item['statistics'].get('commentCount', 0),
+                    'duration': item['contentDetails']['duration']
+                }
+                video_stats.append(stats)
+
+        self.video_stats = video_stats
+        return self
+
+
 if __name__ == "__main__":
+    # Execute in order
     username = "GoogleDevelopers"
-    youtube = YoutubeAPI(username)
-    channel_id = youtube.get_channel_id_by_username()
-    print(channel_id)
+    api = YoutubeAPI(username)
+    api.get_uploads_playlist_id()
+    api.get_video_ids_from_playlist()
+    api.get_video_stats()
